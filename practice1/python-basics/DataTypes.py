2fd1ce8fc8a8a321ffab7e9cb6b93e9492a52b1a
Marat Bekzat <maratbekzat08@gmail.com>
2026-02-05 17:55:57 +0500

    files commit

---
+++ practice1/python-basics/DataTypes.py
@@ -0,0 +1,19 @@
+#1 example:
+x = 5
+print(type(x))
+
+
+#2 example:
+x = frozenset(("apple", "banana", "cherry"))
+
+
+#3 example:
+x = memoryview(bytes(5))
+
+
+#4 example:
+x = bytearray(5)
+
+
+#5 example:
+x = {"apple", "banana", "cherry"}
---
+++ practice1/python-basics/Syntax.py
@@ -0,0 +1,20 @@
+#1 example:
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+#2 example:
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+
+
+#3 example:
+x = 5
+y = "Hello, World!"
+
+
+#4 example:
+#This is a comment.
+print("Hello, World!")
